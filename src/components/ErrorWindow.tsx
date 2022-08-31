65179a3ab232833bf90255901f5fc2264315d8a6
Zicio <zicio@yandex.ru>
2022-08-31 17:07:44 +0400

    Worked

---
+++ src/components/ErrorWindow.tsx
@@ -0,0 +1,9 @@
+const ErrorWindow = () => {
+  return (
+    <div className="bg-red-400 border-[5px] border-solid border-red-600 p-[15px] m-auto">
+      Произошла ошибка
+    </div>
+  );
+};
+
+export default ErrorWindow;
--- src/pages/FormPages/FormPage.tsx
+++ src/pages/FormPages/FormPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import ErrorWindow from "../../components/ErrorWindow";
+import Loader from "../../components/Loader";
 import { IService } from "../../models/models";
 import { useEditServiceQuery } from "../../store/crud/crud.api";
 import "./FormPages.css";
@@ -22,52 +24,61 @@ const FormPage = () => {
 
   return (
     <>
-      {form && (
-        <form className="border border-blue-400 rounded-md shadow-sm shadow-red-600 p-[15px] mt-[40px]">
-          <label className="label mt-[0px]" htmlFor="name">
-            Название
-          </label>
-          <input
-            className="input"
-            name="name"
-            type="text"
-            id="name"
-            value={form.name}
-            onChange={handleChange}
-          />
-          <label className="label" htmlFor="price">
-            Стоимость
-          </label>
-          <input
-            className="input"
-            name="price"
-            type="text"
-            id="price"
-            value={form.price}
-            onChange={handleChange}
-          />
-          <label className="label" htmlFor="content">
-            Описание
-          </label>
-          <input
-            className="input"
-            name="content"
-            type="text"
-            id="content"
-            value={form.content}
-            onChange={handleChange}
-          />
-          <div className="flex justify-end mt-[20px]">
-            <Link to="/">
-              <button className="button bg-red-600" type="submit">
-                Отмена
+      {isLoading && (
+        <div className="m-auto">
+          <Loader />
+        </div>
+      )}
+      {isError ? (
+        <ErrorWindow />
+      ) : (
+        form && (
+          <form className="border border-blue-400 rounded-md shadow-sm shadow-red-600 p-[15px] mt-[40px]">
+            <label className="label mt-[0px]" htmlFor="name">
+              Название
+            </label>
+            <input
+              className="input"
+              name="name"
+              type="text"
+              id="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            <label className="label" htmlFor="price">
+              Стоимость
+            </label>
+            <input
+              className="input"
+              name="price"
+              type="text"
+              id="price"
+              value={form.price}
+              onChange={handleChange}
+            />
+            <label className="label" htmlFor="content">
+              Описание
+            </label>
+            <input
+              className="input"
+              name="content"
+              type="text"
+              id="content"
+              value={form.content}
+              onChange={handleChange}
+            />
+            <div className="flex justify-end mt-[20px]">
+              <Link to="/">
+                <button className="button bg-red-600" type="submit">
+                  Отмена
+                </button>
+              </Link>
+              <button className="button bg-blue-600 ml-[10px]" type="submit">
+                Изменить
               </button>
-            </Link>
-            <button className="button bg-blue-600 ml-[10px]" type="submit">
-              Изменить
-            </button>
-          </div>
-        </form>
+            </div>
+          </form>
+        )
       )}
     </>
   );
